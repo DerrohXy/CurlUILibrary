80277f14b273773fede0a39b1f27d19efe5c3583
Derrick Wafula <deeickwafula1500@gmail.com>
2025-09-11 17:12:28 +0300

    Added individual exports decalartions for components, Initialized documentation

---
+++ README.md
@@ -0,0 +1,7 @@
+# Custom library for CurlUI components.
+
+This library is supposed to provide custom pre-made components for commonly used use cases.
+
+## Components
+
+...
--- post-build.sh
+++ post-build.sh
@@ -6,10 +6,12 @@ cp src/styles.css dist
 
 # Copy package.json files
 icons=(
-  ai bi bs cg ci di fa fa6 fc fi gi go gr hi hi2 im io5
+  bi bs cg ci di fa fa6 fc fi gi go gr hi hi2 im io5
   lia lu md pi ri rx si sl tb tfi ti vsc wi
 )
 
 for icon in "${icons[@]}"; do
   cp "src/icons/$icon/package.json" "dist/icons/$icon"
 done
+
+cp "src/icons/package.json" "dist/icons"
